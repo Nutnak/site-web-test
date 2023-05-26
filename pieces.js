3565b5f86c3007b83d39795ddc777c94b8436699
Quentin Doulcet <quentinucme@hotmail.fr>
2023-05-26 11:49:59 -0400

    ajouté la possibilité de voir si le produit est en stock ou non.

--- pieces.js
+++ pieces.js
@@ -2,10 +2,13 @@ const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json()
 
 
-const article = pieces[4];
+const article = pieces[2];
 
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
+
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
 
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
@@ -17,8 +20,14 @@ prixElement.innerText = `Prix : ${article.prix}€ (${article.prix < 35 ? "€" 
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? 'Aucune categorie';
 
+const stockElement = document.createElement("p");
+stockElement.innerText = `Stock (${article.disponibilite ? "Disponible" : "Indisponible"})`;
+
+
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
