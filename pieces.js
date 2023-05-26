d073e10102dde312a005f59549fe4078397ecc73
Quentin Doulcet <quentinucme@hotmail.fr>
2023-05-26 12:22:45 -0400

    ajouté tous les produits

--- pieces.js
+++ pieces.js
@@ -5,18 +5,20 @@ for (let i = 0; i < pieces.length; i++) {
 
   const article = pieces[i];
 
+  const pieceElement = document.createElement("article")
+
   const imageElement = document.createElement("img");
   imageElement.src = article.image;
 
-  const descriptionElement = document.createElement("p");
-  descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
 
   const nomElement = document.createElement("h2");
   nomElement.innerText = article.nom;
 
+  const descriptionElement = document.createElement("p");
+  descriptionElement.innerText = article.description ?? "Pas de description pour le moment";
+
   const prixElement = document.createElement("p");
   prixElement.innerText = `Prix : ${article.prix}€ (${article.prix < 35 ? "€" : "€€€"})`;
-
 
   const categorieElement = document.createElement("p");
   categorieElement.innerText = article.categorie ?? 'Aucune categorie';
@@ -26,10 +28,11 @@ for (let i = 0; i < pieces.length; i++) {
 
 
   const sectionFiches = document.querySelector(".fiches");
-  sectionFiches.appendChild(imageElement);
-  sectionFiches.appendChild(nomElement);
-  sectionFiches.appendChild(prixElement);
-  sectionFiches.appendChild(categorieElement);
-  sectionFiches.appendChild(descriptionElement);
-  sectionFiches.appendChild(stockElement);
+  sectionFiches.appendChild(pieceElement)
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(stockElement);
 }
